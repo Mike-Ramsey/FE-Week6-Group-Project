e1e3d2e0c460605b2ccc6da0406c09e36ef9f77e
Mark Stearman <cassiemstearman@gmail.com>
2022-03-24 01:16:09 -0500

    added basic User functions

--- groupApp.js
+++ groupApp.js
@@ -35,6 +35,41 @@ class Step {
 
 // Cassie Functions
 
+class User {
+    constructor(name) {
+        this.name = name;       //name of the user
+        this.list;              //assumed to only have one shopping cart
+        this.Recipes = [];      //all recipes that a user can have
+    }
+
+    createList(){
+        this.list = new ShoppingList("Shopping List");  //assumed to only be one shopping list so just name it "shopping list"
+    }
+
+    emptyList(){
+        this.list = NULL;         //make shopping list empty again
+    }
+
+    addRecipe(recipe) {
+        this.Recipes.push(new Recipe(recipe))       //add the recipe by making a new one and pushing it onto the array
+    }
+    
+    deleteRecipe(recipe){
+        this.Recipes.splice(this.Recipes.indexOf(recipe), 1);   //find the index of the recipe that you want to delete
+                                                                //then delete it with splice
+    }
+}
+
+class ShoppingList {
+    constructor (name) {
+        this.name = name;
+        this.ingredients = [];
+    }
+
+    //more functions
+}
+
+
 
 // Erika Functions
 
